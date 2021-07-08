7c057db1fc95eddbcd7c614f1ff53f54db2f111a
Aleksander Machniak <machniak@kolabsys.com>
2021-07-08 14:04:17 +0200

    Add some more tests

---
+++ tests/ParseTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use RtfHtmlPhp\Document;
+use RtfHtmlPhp\Html\HtmlFormatter;
+
+class ParseTest extends TestCase
+{
+  public function testParse1()
+  {
+    $rtf = file_get_contents("tests/rtf/test1.rtf");
+    $document = new Document($rtf);
+    $formatter = new HtmlFormatter();
+    $html = $formatter->Format($document);
+
+    // We only test that it does not throw an exception
+    $this->assertTrue(true);
+  }
+
+  public function testParseException1()
+  {
+    $this->expectException(\Exception::class);
+
+    $document = new Document('{\rtf1\ansi\ansicpg1252\deff0\deflang1046');
+    $formatter = new HtmlFormatter();
+    $html = $formatter->Format($document);
+  }
+}
